e814d07c3640e3b4d2d2013fc7731459b3f1ad83
Christian Straubhaar-Jones <christian.straubhaar@gmail.com>
2017-12-05 18:29:39 -0500

    small changes

--- capdemo/src/App.js
+++ capdemo/src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import './App.css'
+import Header from './Header.js'
+import List from './List.js'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <MuiThemeProvider>
+        <div className='App'>
+          <Header />
+          <List />
+        </div>
+      </MuiThemeProvider>
+    )
   }
 }
 
-export default App;
+export default App
