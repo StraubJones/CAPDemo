bc76649d2e55780e6f5a18bf34bc32154541746f
Christian Straubhaar-Jones <christian.straubhaar@gmail.com>
2017-12-07 08:59:44 -0500

    almost done

--- capdemo/src/Header.js
+++ capdemo/src/Header.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import logo from './logo.png'
+import hero from './Site_Social_image.jpg'
+
+// import logo from './logo.png'
 
 // import './App.css'
 
@@ -7,8 +9,9 @@ class Header extends Component {
   render () {
     return (
       <div className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p className='App-title'>Dream Act Today!</p>
+        {/* <img src={logo} className='App-logo' alt='logo' />
+        <p className='App-title'>Dream Act Today!</p> */}
+        <img src={hero} className='Hero' alt='Dream Act Now. Demand It. No compromises.' />
       </div>
     )
   }
--- capdemo/src/List.js
+++ capdemo/src/List.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import phoneIcon from './phone_icon.png'
 import fullRepsList from './reps.json'
 // import './App.css'
 import {
@@ -11,9 +12,14 @@ import {
 } from 'material-ui/Table'
 import AutoComplete from 'material-ui/AutoComplete'
 
-let states = ['AK - Alaska', 'AL - Alabama', 'AR - Arkansas', 'AS - American Samoa', 'AZ - Arizona', 'CA - California', 'CO - Colorado', 'CT - Connecticut', 'DC - District of Columbia', 'DE - Delaware', 'FL - Florida', 'GA - Georgia', 'GU - Guam', 'HI - Hawaii', 'IA - Iowa', 'ID - Idaho', 'IL - Illinois', 'IN - Indiana', 'KS - Kansas', 'KY - Kentucky', 'LA - Louisiana', 'MA - Massachusetts', 'MD - Maryland', 'ME - Maine', 'MI - Michigan', 'MN - Minnesota', 'MO - Missouri', 'MS - Mississippi', 'MT - Montana', 'NC - North Carolina', 'ND - North Dakota', 'NE - Nebraska', 'NH - New Hampshire', 'NJ - New Jersey', 'NM - New Mexico', 'NV - Nevada', 'NY - New York', 'OH - Ohio', 'OK - Oklahoma', 'OR - Oregon', 'PA - Pennsylvania', 'PR - Puerto Rico', 'RI - Rhode Island', 'SC - South Carolina', 'SD - South Dakota', 'TN - Tennessee', 'TX - Texas', 'UT - Utah', 'VA - Virginia', 'VI - Virgin Islands', 'VT - Vermont', 'WA - Washington', 'WI - Wisconsin', 'WV - West Virginia', 'WY - Wyoming']
+let states = ['Show All', 'AK - Alaska', 'AL - Alabama', 'AR - Arkansas', 'AS - American Samoa', 'AZ - Arizona', 'CA - California', 'CO - Colorado', 'CT - Connecticut', 'DC - District of Columbia', 'DE - Delaware', 'FL - Florida', 'GA - Georgia', 'GU - Guam', 'HI - Hawaii', 'IA - Iowa', 'ID - Idaho', 'IL - Illinois', 'IN - Indiana', 'KS - Kansas', 'KY - Kentucky', 'LA - Louisiana', 'MA - Massachusetts', 'MD - Maryland', 'ME - Maine', 'MI - Michigan', 'MN - Minnesota', 'MO - Missouri', 'MS - Mississippi', 'MT - Montana', 'NC - North Carolina', 'ND - North Dakota', 'NE - Nebraska', 'NH - New Hampshire', 'NJ - New Jersey', 'NM - New Mexico', 'NV - Nevada', 'NY - New York', 'OH - Ohio', 'OK - Oklahoma', 'OR - Oregon', 'PA - Pennsylvania', 'PR - Puerto Rico', 'RI - Rhode Island', 'SC - South Carolina', 'SD - South Dakota', 'TN - Tennessee', 'TX - Texas', 'UT - Utah', 'VA - Virginia', 'VI - Virgin Islands', 'VT - Vermont', 'WA - Washington', 'WI - Wisconsin', 'WV - West Virginia', 'WY - Wyoming']
 let updatedRepsList = fullRepsList.map((rep) => {
   rep.imgurl = process.env.PUBLIC_URL + '/Project_Materials/shareable_images/' + rep.imgurl
+  console.log(rep.tweeturl)
+  let tweeturlArray = rep.tweeturl.split('%20')
+  rep.tweeturl = tweeturlArray[0].split('=')[0] + '=' + 'Lorem ipsum dolor sit met'.split(' ').join('%20')
+  rep.facebookurl = 'https://www.facebook.com/dialog/feed?app_id=184683071273&link=http%3A%2F%2Fwww.insert-url-to-share-here.com&picture=http%3A%2F%2Fwww.insert-image-share-url-here.jpg&name=This%20is%20a%20Sample%20Title&caption=%20&description=consectetur%20adipiscing%20elit&redirect_uri=http%3A%2F%2Fwww.facebook.com%2F'
+  console.log(rep.tweeturl)
   return rep
 })
 
@@ -26,12 +32,16 @@ class List extends Component {
     }
   }
   filterReps (chosen, i) {
-    if (chosen.length) {
+    if (chosen.length && chosen !== 'Show All') {
       let filteredReps = updatedRepsList.filter((rep) => {
         return rep.name.includes(chosen.slice(0, 2))
       })
       this.setState({
         shownReps: filteredReps
+      })
+    } else {
+      this.setState({
+        shownReps: updatedRepsList
       })
     }
   }
@@ -39,7 +49,6 @@ class List extends Component {
     return (
       <div>
         <div className='AutoComplete'>
-          <h2>Contact your representatives about the Dream Act</h2>
           <AutoComplete
             floatingLabelText='Enter your state by name or abbrev.'
             filter={AutoComplete.fuzzyFilter}
@@ -49,20 +58,27 @@ class List extends Component {
           />
         </div>
         <Table>
-          <TableBody>
+          <TableBody displayRowCheckbox={false} stripedRows>
             {this.state.shownReps.length
               ? this.state.shownReps.map((rep, i) => {
                 return (
                   <TableRow key={i}>
-                    <TableRowColumn>
-                      <img className='RepImage' src={rep.imgurl} />
+                    <TableRowColumn className='TableRow'>
+                      {/*
+                        <img className='RepImage' src={rep.imgurl} />
+                      */}
                       <p>{rep.name}</p>
                     </TableRowColumn>
-                    <TableRowColumn>
-                      <a href={rep.phoneno}>{rep.phoneno}</a>
+                    <TableRowColumn className='TableRow'>
+                      <a href={rep.phoneno}>
+                        <img className='PhoneIcon' alt='telephone' src={phoneIcon} />
+                        {rep.phoneno}
+                      </a>
                     </TableRowColumn>
-                    <TableRowColumn>
-                      <a href={rep.tweeturl}>Tweet at this rep!</a>
+                    <TableRowColumn className='TableRow'>
+                      <i className='fa fa-twitter' aria-hidden='true' />
+                      <a className='TwitterCall' href={rep.tweeturl}>Tweet at this rep!</a>
+                      <div className='fb-share-button' data-href='http://localhost:3000/CAP' data-layout='button_count' data-size='small' data-mobile-iframe='true'><a className='fb-xfbml-parse-ignore' target='_blank' href='https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Flocalhost%3A3000%2FCAP&amp;src=sdkpreparse'>Share</a></div>
                     </TableRowColumn>
                   </TableRow>
                 )
